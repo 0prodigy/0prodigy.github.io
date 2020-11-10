9a3466b2db01438e002e5e05b87cf0cecdd03cb1
Akash <pathakvikash9211@gmail.com>
2020-11-10 15:21:42 +0530

    Updated projects

---
+++ precache-manifest.bb96dbcf13ef8d7e7d2adcda7f750444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530be7b0ada3b6b970999c3b92cf2c82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db643c8c6d9a8a1cf900",
+    "url": "./static/css/main.b53c0810.chunk.css"
+  },
+  {
+    "revision": "53d5ee912f3b6a47616b",
+    "url": "./static/js/2.ce896b69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ce896b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db643c8c6d9a8a1cf900",
+    "url": "./static/js/main.bc507609.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "73b9994bb505da43a387d107849baf95",
+    "url": "./static/media/MonumentExtended-Regular.73b9994b.otf"
+  }
+]);
